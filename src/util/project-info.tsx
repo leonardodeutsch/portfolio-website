5885428489ee102377ae4a7fe5cc9b4a535c8013
Leo <deutschleonardo@gmail.com>
2022-02-17 08:19:58 -0600

    added repo links

--- src/util/project-info.tsx
+++ src/util/project-info.tsx
@@ -9,28 +9,28 @@ const projects = [
     logo: pantryChefLogo,
     tech: 'React | HTML/CSS3 | Node.js | Express | PostgreSQL | AWS | Docker',
     description: 'A collaborative cooking recipe application with user specific customization.',
-    links: 'https://github.com/'
+    links: 'https://github.com/hack-curtains/project-gordon-web'
   },
   {
     title: 'SDC',
     logo: SDCLogo,
     tech: 'Node.js | Express | MongoDB | Mongoose | Artillery.io | Loader.io | NGINX | AWS | Docker',
     description: 'Complete scalable back-end system design for a retail website with over 50 million product entries.',
-    links: 'https://github.com/'
+    links: 'https://github.com/Poplin-Devs/products-service'
   },
   {
     title: 'Catwalk',
     logo: catwalkLogo,
     tech: 'React | HTML/CSS3 | Node.js | Express | AWS',
     description: 'A modern, client-facing retail web portal design for an apparel website.',
-    links: 'https://github.com/'
+    links: 'https://github.com/team-soapstone/front-end-capstone'
   },
   {
     title: 'League Matchup',
     logo: leagueMatchupLogo,
     tech: 'React | HTML/CSS3 | MongoDB | Mongoose | Node.js | Express | AWS | Riot Games API',
     description: 'An application for the popular videogame League of Legends, where players can come together and share their knowledge on specific champion matchups.',
-    links: 'https://github.com/'
+    links: 'https://github.com/leonardodeutsch/league-matchup'
   }
 ];
 
